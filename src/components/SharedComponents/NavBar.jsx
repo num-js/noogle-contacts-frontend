c717953a20c0593767b04afe1e2831e4106b585e
Numan Ahmed <mdnmnahmed@gmail.com>
2022-04-10 07:41:59 +0530

    Contacts Search Functionality

--- src/components/SharedComponents/NavBar.jsx
+++ src/components/SharedComponents/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import { ADD_CONTACT } from '../../utils/routerLinks';
 
-const NavBar = () => {
+const NavBar = ({ searchContact, setSearchContact }) => {
     return (
         <div>
             <nav className="navbar shadow fixed-top navbar-expand-sm navbar-dark nbg-primary">
@@ -14,12 +14,13 @@ const NavBar = () => {
                     </Link>
                     <div>
                         <div class="d-flex justify-content-center align-items-center" style={{}}>
-                            {/* <div class=""> */}
                             <div class="search">
-                                <input type="text" class="form-control" placeholder="Search" />
+                                <input type="text" class="form-control" placeholder="Search"
+                                    value={searchContact}
+                                    onChange={(event) => setSearchContact(event.target.value)}
+                                />
                                 <button class="btn nbg-primary"><i class="fa fa-search"></i>N</button>
                             </div>
-                            {/* </div> */}
                         </div>
                     </div>
                     <div>
