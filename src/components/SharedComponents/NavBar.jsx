aef60f66ec06dc7d07b5fc3a2b271f6aa7bcf44d
Numan Ahmed <mdnmnahmed@gmail.com>
2022-04-10 07:20:54 +0530

    Search UI

--- src/components/SharedComponents/NavBar.jsx
+++ src/components/SharedComponents/NavBar.jsx
@@ -5,18 +5,28 @@ import { ADD_CONTACT } from '../../utils/routerLinks';
 const NavBar = () => {
     return (
         <div>
-            <Link to="/">
-                <nav className="navbar shadow fixed-top navbar-expand-sm navbar-dark nbg-primary">
-                    <div className="container">
+            <nav className="navbar shadow fixed-top navbar-expand-sm navbar-dark nbg-primary">
+                <div className="container">
+                    <Link to="/">
                         <div role="button" className="navbar-brand">
                             Noogle<i className="material-icons">account_circle</i>Contacts
                         </div>
-                        <div>
-                            <Link to={ADD_CONTACT} className="btn btn-info btn-sm ml-auto shadow rounded-0"><i className="fa fa-user-plus shadow"></i> Add Contact</Link>
+                    </Link>
+                    <div>
+                        <div class="d-flex justify-content-center align-items-center" style={{}}>
+                            {/* <div class=""> */}
+                            <div class="search">
+                                <input type="text" class="form-control" placeholder="Search" />
+                                <button class="btn nbg-primary"><i class="fa fa-search"></i>N</button>
+                            </div>
+                            {/* </div> */}
                         </div>
                     </div>
-                </nav>
-            </Link>
+                    <div>
+                        <Link to={ADD_CONTACT} className="btn btn-info btn-sm ml-auto shadow rounded-0"><i className="fa fa-user-plus shadow"></i> Add Contact</Link>
+                    </div>
+                </div>
+            </nav>
         </div>
     )
 }
